84fb13500e10f5fd1d4fd081f2903b7169fafe02
shaun <shaun@shaunmcfall.com>
2018-11-11 15:57:42 -0500

    Initial commit

---
+++ Source/ExpandoZoneMod.cs
@@ -0,0 +1,40 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace Merthsoft.ExpandoZone {
+    public class ExpandoZoneMod : Mod {
+        public static HarmonyInstance HarmonyInstance { get; set; }
+
+        public static Texture2D Icon_Expand { get; private set; }
+
+        static HashSet<Zone_Stockpile> expandoStockpileSet { get; set; }
+
+        static ExpandoZoneMod() {
+            HarmonyInstance = HarmonyInstance.Create("Merthsoft.ExpandoZone");
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+
+            LongEventHandler.ExecuteWhenFinished(() => {
+                Icon_Expand = ContentFinder<Texture2D>.Get("UI/Commands/Expand", true);
+            });
+
+            expandoStockpileSet = new HashSet<Zone_Stockpile>();
+        }
+
+        public ExpandoZoneMod(ModContentPack content) : base(content) {
+
+        }
+
+        public static void AddZone(Zone_Stockpile zone) => expandoStockpileSet.Add(zone);
+
+        public static void RemoveZone(Zone_Stockpile zone) => expandoStockpileSet.Remove(zone);
+
+        public static bool ContainsZone(Zone_Stockpile zone) => expandoStockpileSet.Contains(zone);
+    }
+}
---
+++ Source/Patches/Zone_Stockpile_GetGizmos.cs
@@ -0,0 +1,29 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using static Merthsoft.ExpandoZone.ExpandoZoneMod;
+
+namespace Merthsoft.ExpandoZone.Patches {
+    [HarmonyPatch(typeof(Zone_Stockpile), "GetGizmos")]
+    public class Zone_Stockpile_GetGizmos {
+        static void Postfix(IEnumerable<Gizmo> __result, Zone_Stockpile __instance) {
+            __result.Add(new Command_Toggle() {
+                icon = Icon_Expand,
+                defaultLabel = "Auto-Expand",
+                defaultDesc = "Automatically expand to fill a room",
+                isActive = () => ContainsZone(__instance),
+                toggleAction = () => {
+                    if (ContainsZone(__instance)) {
+                        RemoveZone(__instance);
+                    } else {
+                        AddZone(__instance);
+                    }
+                } 
+            });
+        }
+    }
+}
